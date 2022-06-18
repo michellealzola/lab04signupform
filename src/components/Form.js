9585631f03f0835ed1a7af2210475ad042df4a35
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2022-06-18 10:26:54 -0600

    Added Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         username: '',
+         password: '',         
+      }
+    }
+
+    handleUserNameChange = (event) =>{
+        this.setState({
+            username : event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) =>{
+        this.setState({
+            password : event.target.value
+        })
+    }
+    
+    handleSubmit = (event) =>{                   
+        event.preventDefault();
+        
+        const userInList = users.find((user) => user.username === this.state.username);
+
+        if(userInList)
+        {
+            if(userInList.password !== this.state.password)
+            {
+                alert('Incorrect Password');
+            }
+            else
+            {
+                alert(`${this.state.username}
+                ${this.state.password}`);
+            }
+        }
+        else
+        {
+            alert('Incorrect User Name');
+        }
+    }
+    
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <div>
+                <label>Username </label>
+                <input type='text' value={this.state.username} onChange={this.handleUserNameChange}></input>
+            </div>
+            <div>
+                <label>Password </label>
+                <input type='text' value={this.state.password} onChange={this.handlePasswordChange}></input>
+            </div>
+            <div className='btn'>
+                <button>Submit</button>
+            </div>            
+            
+        </form>
+        
+    )
+  }
+}
+
+export default Form
